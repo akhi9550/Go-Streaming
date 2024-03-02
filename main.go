1723cad8cb8ccb82766881fd700962cb487529c1
akhi9550 <akhilc248@gmail.com>
2024-03-02 20:22:13 +0530

    simple streaming

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	const songsDir = "video"
+	const port = 8000
+
+	http.Handle("/", addHeaders(http.FileServer(http.Dir(songsDir))))
+	fmt.Printf("Starting server on %v\n", port)
+	log.Printf("Serving %s on HTTP port: %v\n", songsDir, port)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+func addHeaders(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		h.ServeHTTP(w, r)
+	}
+}
